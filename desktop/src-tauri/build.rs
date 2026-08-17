f8c2526848d2952d4ab7233309053db3f117856a
xDragos <64187224+xDragos@users.noreply.github.com>
2026-08-17 21:01:51 +0300

    Update build.rs

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+
+    let target = std::env::var("TARGET").unwrap_default();
+    if target.contains("apple-ios") {
+        println!("cargo:rustc-link-lib=framework=MediaPlayer");
+        println!("cargo:rustc-link-lib=framework=AVFoundation");
+        println!("cargo:rustc-link-lib=framework=CoreAudio");
+        println!("cargo:rustc-link-lib=framework=AudioToolbox");
+    }
 }
