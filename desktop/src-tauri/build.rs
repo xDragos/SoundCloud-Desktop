dd4c007c62675a63e953dd82d6365859aeb22ae2
xDragos <64187224+xDragos@users.noreply.github.com>
2026-08-17 21:59:18 +0300

    Update build.rs

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -7,8 +7,5 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=AVFoundation");
         println!("cargo:rustc-link-lib=framework=CoreAudio");
         println!("cargo:rustc-link-lib=framework=AudioToolbox");
-        
-        // Asigură că simbolurile din librăria statică souvlaki nu sunt eliminate de dead-code elimination
-        println!("cargo:rustc-link-arg=-ObjC");
     }
 }
